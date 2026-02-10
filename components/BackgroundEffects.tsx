54687e224633aaf752592982c0ba07da04055166
draftworks-repo <draftworks000@gmail.com>
2026-02-11 02:26:38 +0530

    background effect sign updated

--- components/BackgroundEffects.tsx
+++ components/BackgroundEffects.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './BackgroundEffects.css';
+import React from "react";
+import "./BackgroundEffects.css";
 
 export const BackgroundEffects: React.FC = () => {
   return (
@@ -13,8 +13,17 @@ export const BackgroundEffects: React.FC = () => {
       <div className="grid-plane"></div>
 
       {/* Decorative 3D Shapes */}
-      <div className="floating-shape shape-capsule"></div>
-      <div className="floating-shape shape-spiral"></div>
+      {/* <div className="floating-shape shape-capsule"></div> */}
+      <div className="floating-shape shape-code-brackets">
+        <span className="bracket-right"></span>
+        <span className="slash"></span>
+        <span className="bracket-left"></span>
+      </div>
+
+      {/* <div className="floating-shape shape-spiral"></div> */}
+      <div className="floating-shape shape-video-timeline">
+        <div className="playhead"></div>
+      </div>
 
       <div className="noise-overlay"></div>
     </div>
