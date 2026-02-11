99b214a15ab6e52fb599061ac9df0ff46f914f96
draftworks-repo <draftworks000@gmail.com>
2026-02-11 21:09:29 +0530

    new component and order arranged in home

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "../components/Hero";
 import { BackgroundEffects } from "../components/BackgroundEffects";
 import { ServicesBento } from "../components/ServicesBento";
+import { ExtendedCapabilities } from "../components/ExtendedCapabilities";
 import { OverviewSection } from "../components/OverviewSection";
 import { ClientsMarquee } from "../components/ClientsMarquee";
 import { AboutSection } from "../components/AboutSection";
@@ -8,6 +9,8 @@ import { CallToAction } from "../components/CallToAction";
 import { Footer } from "../components/Footer";
 import ComingSoon from "../components/ComingSoon";
 import PageTransition from "../components/PageTransition";
+import ReviewCarousel from "../components/ReviewCarousel";
+import StepComponentTwo from "../components/StepCompoenntTwo";
 
 export default function Home() {
   return (
@@ -32,10 +35,19 @@ export default function Home() {
             <ServicesBento />
           </section>
 
-          <OverviewSection />
+          <section id="process">
+            <StepComponentTwo />
+          </section>
 
           <ClientsMarquee direction="left" />
-          <ClientsMarquee direction="right" />
+
+          <ReviewCarousel />
+
+          <section id="extended-capabilities">
+            <ExtendedCapabilities />
+          </section>
+
+          <OverviewSection />
 
           <AboutSection />
 
