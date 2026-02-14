028c00d8c413f061b4e826feb16fe1449edc63cc
draftworks-repo <draftworks000@gmail.com>
2026-02-14 17:53:48 +0530

    Call to action finalised

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -10,24 +10,38 @@ export const CallToAction: React.FC = () => {
 
       <div className="cta-container">
         <h2 className="cta-title">
-          Crafting clean, scalable web applications.
+          Ready to <span className="build">Build </span> Something That{" "}
+          <span className="performs">Performs</span> and{" "}
+          <span className="scales">Scales?</span>
         </h2>
         <p className="cta-subtitle">
-          Join teams who build modern web products and fast-growing startups.
+          If you’re planning a new website or web application,
           <br className="desktop-br" />
-          that grow without sacrificing code quality.
+          let’s turn your vision into a structured, high-performing solution.
         </p>
 
-        <a
-          href="/contact"
-          className="cta-main-button"
-          style={{ textDecoration: "none" }}
-        >
-          <span className="cta-button-label">Get Started- It's Free</span>
-          <div className="cta-button-icon-wrapper">
-            <ArrowUpRight size={18} />
-          </div>
-        </a>
+        <div className="cta-button-container">
+          <a
+            href="/contact"
+            className="cta-main-button secondary"
+            style={{ textDecoration: "none" }}
+          >
+            <span className="cta-button-label secondary">Schedule a Call</span>
+            <div className="cta-button-icon-wrapper secondary">
+              <ArrowUpRight size={18} />
+            </div>
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=918759475316&text=Hey%20Talib%2C%20I%20want%20start%20my%20project"
+            className="cta-main-button"
+            style={{ textDecoration: "none" }}
+          >
+            <span className="cta-button-label">Start Your Project</span>
+            <div className="cta-button-icon-wrapper">
+              <img src="images/whatsapp.svg" alt="" className="cta-whatsapp" />
+            </div>
+          </a>
+        </div>
 
         {/* 3D Globe Visual Integration */}
         <div className="cta-visual-decoration">
